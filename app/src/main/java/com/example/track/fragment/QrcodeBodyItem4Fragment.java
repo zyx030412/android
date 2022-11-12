7db1e33f936a20f61b813cf50d5893b28503415d
zyx030412 <1434039075@qq.com>
2022-11-12 12:47:35 +0800

    2.1 Track 保存

--- app/src/main/java/com/example/track/fragment/QrcodeBodyItem4Fragment.java
+++ app/src/main/java/com/example/track/fragment/QrcodeBodyItem4Fragment.java
@@ -1,6 +1,8 @@
 package com.example.track.fragment;
 
 import android.graphics.Color;
+import android.media.MediaParser;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,13 +17,17 @@ import androidx.fragment.app.Fragment;
 
 import com.example.track.R;
 import com.example.track.model.View.CirStatisticGraph;
+import com.example.track.model.View.CurrentTemperatureView;
 import com.example.track.model.View.WaveView;
+
+import okhttp3.MediaType;
 
 public class QrcodeBodyItem4Fragment extends Fragment {
     private CirStatisticGraph cirStatisticGraph;
-    private View mCurrentTemperature;
+    private CurrentTemperatureView mCurrentTemperature;
     private WaveView waveview;
     private float waterPrecent;//范围0.00--1.00
+    private MediaPlayer mMediaPlayer;
 
     @Nullable
     @Override
@@ -29,6 +35,7 @@ public class QrcodeBodyItem4Fragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_body_qrcode_item4, container, false);
 
         init(v);
+        SoundInit();
         //设置水位百分比
         waveview.setPrecentChangeListener(new WaveView.PrecentChangeListener() {
             @Override
@@ -40,10 +47,25 @@ public class QrcodeBodyItem4Fragment extends Fragment {
         return v;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        SoundInit();
+        if(Double.parseDouble(mCurrentTemperature.getCurTemp())>=28){
+            mMediaPlayer.start();
+        }
+    }
+
+    public void SoundInit(){
+        mMediaPlayer = MediaPlayer.create(getContext(), R.raw.temperature);
+    }
+
+
     public void init(View v){
 
         mCurrentTemperature = v.findViewById(R.id.view_221);
         LinearLayout layout_view = v.findViewById(R.id.activity_currentTemp_view);
+
 
 
         waveview = v.findViewById(R.id.waveview);
